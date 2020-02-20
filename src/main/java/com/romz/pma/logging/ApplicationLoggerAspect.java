b120cb6a26c71c031615738b7e04336acdee80bf
Roman Z <zroman88@gmail.com>
2020-02-20 07:37:31 +0300

    o AOP

---
+++ src/main/java/com/romz/pma/logging/ApplicationLoggerAspect.java
@@ -0,0 +1,36 @@
+package com.romz.pma.logging;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/**
+ * @author roman - Project project-management
+ */
+@Aspect
+@Component
+public class ApplicationLoggerAspect {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Pointcut("within(com.romz.pma.controllers..*)")
+    public void definePackagePointcuts() {
+        // empty method to name the location specified in the pointcut
+    }
+
+    @After("definePackagePointcuts()")
+    public void logBefore(JoinPoint jp) {
+        log.debug("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        log.debug(("-------Before Method Execution------- \n {}.{}() {}"),
+                  jp.getSignature().getDeclaringType(),
+                  jp.getSignature().getName(),
+                  Arrays.toString(jp.getArgs()));
+        log.debug("---------------------------------------------");
+    }
+}
